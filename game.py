ed9975c7d7044657a63a734178a987bffaa2a6de
chaimaa <cm3568@stern.nyu.edu>
2019-06-05 19:30:17 -0400

    refactor or simplify

--- game.py
+++ game.py
@@ -14,7 +14,9 @@ print("USER CHOICE", user_choice)  # Making sure the input is captured properly
 
 # VALIDATE INPUTS
 
-if user_choice not in ["rock", "paper", "scissors"]:
+options = ["rock", "paper", "scissors"]
+
+if user_choice not in options:
     print("INVALID SELECTION, PLEASE TRY AGAIN...")
     exit()
 
@@ -22,7 +24,7 @@ if user_choice not in ["rock", "paper", "scissors"]:
 
 print("GENERATING...")
 
-computer_choice = random.choice(["rock", "paper", "scissors"])
+computer_choice = random.choice(options)
 
 print("----------")
 print("COMPUTER CHOICE", computer_choice)
