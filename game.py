8203cde5f6ad2e31b6ab66651b3d7a027ff785fd
chaimaa <cm3568@stern.nyu.edu>
2019-06-05 18:59:20 -0400

    game.py change input
    outline

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
+# game.py
+
 print("Rock, Paper, Scissors, Shoot!")
+
+# CAPTURE INPUTS
+
+user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
+
+print("----------")
+print("USER CHOICE", user_choice)  # Making sure the input is captured properly
+
+# VALIDATE INPUTS
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUTS / OUTCOMES
+
