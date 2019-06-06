e14e977f82cf6f83de9244612882d85da67deef7
chaimaa <cm3568@stern.nyu.edu>
2019-06-05 20:21:21 -0400

    added alternatives to computer selection
    nested disctionaries

--- game.py
+++ game.py
@@ -38,20 +38,60 @@ print("COMPUTER CHOICE", computer_choice)
 # number of possible outcomes is 9 including the ties 
 #
 
-if user_choice == computer_choice:
-    print("IT'S A TIE! PLAY AGAIN!")
-elif user_choice == "rock" and computer_choice == "paper":
-    print("AND THE WINNER IS ... PAPER")
-elif user_choice == "rock" and computer_choice == "scissors":
-    print("AND THE WINNER IS ... ROCK")
-elif user_choice == "paper" and computer_choice == "rock":
-    print("AND THE WINNER IS ... PAPER")
-elif user_choice == "paper" and computer_choice == "scissors":
-    print("AND THE WINNER IS ... SCISSORS")
-elif user_choice == "scissors" and computer_choice == "rock":
-    print("AND THE WINNER IS ... ROCK")
-elif user_choice == "scissors" and computer_choice == "paper":
-    print("AND THE WINNER IS ... SCISSORS")
+#if user_choice == computer_choice:
+#    print("IT'S A TIE! PLAY AGAIN!")
+#elif user_choice == "rock" and computer_choice == "paper":
+#    print("AND THE WINNER IS ... PAPER")
+#    print("YOU LOSE!")
+#elif user_choice == "rock" and computer_choice == "scissors":
+#    print("AND THE WINNER IS ... ROCK")
+#    print("YOU WIN!")
+#elif user_choice == "paper" and computer_choice == "rock":
+#    print("AND THE WINNER IS ... PAPER")
+#    print("YOU WIN!")
+#elif user_choice == "paper" and computer_choice == "scissors":
+#    print("AND THE WINNER IS ... SCISSORS")
+#    print("YOU LOSE!")
+#elif user_choice == "scissors" and computer_choice == "rock":
+#    print("AND THE WINNER IS ... ROCK")
+#    print("YOU LOSE!")
+#elif user_choice == "scissors" and computer_choice == "paper":
+   #print("AND THE WINNER IS ... SCISSORS")
+   #print("YOU WIN!")
+
+
+winners = {
+    "rock":{
+        "rock": None,
+        "paper": "paper",
+        "scissors": "rock",
+    },
+    "paper":{
+        "rock": "paper",
+        "paper": None,
+        "scissors": "scissors",
+    },
+    "scissors":{
+        "rock": "rock",
+        "paper": "scissors",
+        "scissors": None,
+    },
+}
+
+
+winning_choice = winners[user_choice][computer_choice]
+
+
+if winning_choice:
+    if winning_choice == user_choice:
+        print("YOU WON")
+    elif winning_choice == computer_choice:
+        print("YOU LOST")
+    else:
+        print("TIE")
+else:
+    print("TIE")
+
 
 
 
